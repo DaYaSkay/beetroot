f18052949fce22ece4d0019c5f69d2fcc665d2fe
Your Name <you@example.com>
2022-10-12 18:29:45 +0200

    123

---
+++ lesson3.cs
@@ -0,0 +1,19 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            if (a > b)
+            {
+                for (int i = b; i <= a; i++)
+                {
+                    sum += i;
+                }
+
+            }
+            else
+            {
+                for (int i = a; i <= b; i++)
+                {
+                    sum += i;
+                }
+            }
+            Console.Write(sum);
